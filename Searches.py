2a6884978c1924f7bf5218bd516facb2014ea37a
Shivansh Chhabra <20603692+shan18u@users.noreply.github.com>
2023-01-02 19:24:44 -0800

    Binary search

--- Searches.py
+++ Searches.py
@@ -1,4 +1,4 @@
-
+#Linear Search
 def searches(arr, target):      #call function
   for i in range(len(arr)):       # run for loop, run index one-by-one and incerement i++, until we find our target which is 3
     if arr[i] == target:            # once we find target return the index or i
@@ -6,7 +6,27 @@ def searches(arr, target):      #call function
     else:
       continue
       
-arr= [1, 2, 3, 4, 5]
+arr= [2, 1, 3, 6, 5, 4]
 target = 3
 print(searches(arr, target))
 
+
+# Binary Search          Array is already sorted before it starts search
+def searches(arr, start, end, target):          # call the function
+  while (start <= end):                            # if start is < or = end, make mid
+    mid = (start + end) // 2                      
+      
+    if mid > target:                                  # if mid > target, then start arr from right bcz array is already sorted unlike linear
+      start = mid + 1
+      
+    elseif mid < target:
+      start = mid - 1
+      
+    else:
+      return mid
+    
+return start                                   # return the answer that we go from either if or elseif
+
+arr= [1, 2, 3, 4, 5]
+target = 3
+print(searches(arr, 0, len(arr) -1, target))        # arr, i = 0, i = last index, 3
