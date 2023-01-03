18464e0d08e445a4d6b016a13d35346ed9f3a8f2
Shivansh Chhabra <20603692+shan18u@users.noreply.github.com>
2023-01-02 19:03:34 -0800

    Linear search.

--- Searches.py
+++ Searches.py
@@ -1,1 +1,12 @@
 
+def searches(arr, target):      #call function
+  for i in range(len(arr)):       # run for loop, run index one-by-one and incerement i++, until we find our target which is 3
+    if arr[i] == target:            # once we find target return the index or i
+      return i
+    else:
+      continue
+      
+arr= [1, 2, 3, 4, 5]
+target = 3
+print(searches(arr, target))
+
